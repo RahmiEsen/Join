26ab632e1fb0ce5f432e81e2b581b2aa400077b8
Rahmi Esen <esenrahmi23@gmail.com>
2025-06-03 18:44:51 +0200

    Add SignUp form layout and styling

--- src/app/features/signup/signup.component.ts
+++ src/app/features/signup/signup.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
 
+export class SignupComponent {
+  formFields = [
+    {
+      type: 'text',
+      placeholder: 'Name',
+      icon: 'person.png',
+      alt: 'Person'
+    },
+    {
+      type: 'email',
+      placeholder: 'Email',
+      icon: 'mail.png',
+      alt: 'Mail'
+    },
+    {
+      type: 'password',
+      placeholder: 'Password',
+      icon: 'lock.png',
+      alt: 'Password'
+    },
+    {
+      type: 'password',
+      placeholder: 'Confirm Password',
+      icon: 'lock.png',
+      alt: 'Confirm Password'
+    }
+  ];
 }
