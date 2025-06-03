1d14c19bd2c6f54ea20f5736a7817c3c6c901cdc
Rahmi Esen <esenrahmi23@gmail.com>
2025-06-03 14:15:49 +0200

    Setup routing for signup feature with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: 'signup',
+    loadChildren: () =>
+        import('./features/signup/signup.routes').then(m => m.SIGNUP_ROUTES)
+    },
+    {
+        path: '',
+        redirectTo: 'signup',
+        pathMatch: 'full'
+    }
+];
