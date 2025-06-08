2eb8a46828b1d831a0e9df1f86a8a2f0305c9100
Rahmi Esen <esenrahmi23@gmail.com>
2025-06-08 03:32:31 +0200

    Implement signup & login with hashed passwords using Prisma

--- Backend/api/src/auth/auth.service.ts
+++ Backend/api/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+    
+    async signup(email: string, password: string) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await this.prisma.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+        },
+        });
+        return { message: 'User created', userId: user.id };
+    }
+}
