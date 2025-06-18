ae6bdb6e031095835a06be098e033d2ae66d7a5f
Rahmi Esen <esenrahmi23@gmail.com>
2025-06-18 05:01:36 +0200

    feat: implement sidebar layout

--- Frontend/src/app/core/layout/sidebar/sidebar.component.ts
+++ Frontend/src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 
 export class SidebarComponent {
-
+  navItems = [
+    {
+      label: 'Summary',
+      route: '/summary',
+      icon: 'summary.png',
+      iconHover: 'summary-hover.png'
+    },
+    {
+      label: 'Add Task',
+      route: '/add-task',
+      icon: 'add-task.png',
+      iconHover: 'add-task-hover.png'
+    },
+    {
+      label: 'Board',
+      route: '/board',
+      icon: 'board.png',
+      iconHover: 'board-hover.png'
+    },
+    {
+      label: 'Contacts',
+      route: '/contacts',
+      icon: 'contacts.png',
+      iconHover: 'contacts-hover.png'
+    }
+  ];
+  hoveredItem: string | null = null;
 }
