617e0f582cb93fe0b88af73f7b6e63beccaaa62b
Rahmi Esen <esenrahmi23@gmail.com>
2025-06-19 13:21:26 +0200

    feat: generate remaining feature components (add-task, board, contacts, help, privacy-policy, legal-notice)

--- Frontend/src/app/core/layout/header/header.component.ts
+++ Frontend/src/app/core/layout/header/header.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import { dropdownAnimation } from '../../../shared/animations/dropdown.animation';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../shared/services/auth.service';
+
 
 @Component({
   selector: 'app-header',
@@ -15,13 +16,25 @@ import { AuthService } from '../../../shared/services/auth.service';
 
 export class HeaderComponent {
   showDropdown = false;
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private eRef: ElementRef
+  ) {}
   
   toggleDropdown() {
     this.showDropdown = !this.showDropdown;
   }
+  
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/auth/login']);
+  }
+  
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event): void {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.showDropdown = false;
+    }
   }
 }
