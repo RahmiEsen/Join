6283cd3c412ef0cd851c4d30c1a2dddd9c92a54b
Rahmi Esen <esenrahmi23@gmail.com>
2025-08-27 15:05:23 +0200

    fix: configure CORS to allow frontend origin

--- Backend/api/src/main.ts
+++ Backend/api/src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://join.rahmiesen.de',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
