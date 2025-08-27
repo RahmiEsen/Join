55db6dc4bf059b30e263bd635be13bec4cad4f5f
Rahmi Esen <esenrahmi23@gmail.com>
2025-08-27 21:16:47 +0200

    fixed main.ts backend

--- Backend/api/src/main.ts
+++ Backend/api/src/main.ts
@@ -3,15 +3,18 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create(AppModule, { cors: false });
+
   app.enableCors({
-    origin: 'https://join.rahmiesen.de',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: ['https://join.rahmiesen.de', 'http://localhost:4200'],
+    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204, // saubere OPTIONS-Antwort
   });
-  
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
